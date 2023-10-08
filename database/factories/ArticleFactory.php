4c2fae3f30e5d91ed10b8b167996226c49202a4d
Snooper <snooper7@yandex.ru>
2023-10-08 09:00:37 +0300

    env installed

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->sentence(6, true);
+        $slug = Str::substr(Str::lower(preg_replace('/\s+/', '-', $title)), 0 ,-1);
+
+        return [
+            'title' => $title,
+            'body' => $this->faker->paragraph(100, true),
+            'slug' => $slug,
+            'img' => 'https://loremflickr.com/600/600/php',
+            'created_at' => $this->faker->dateTimeBetween('-1 years')
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
+use App\Models\Comment;
+use App\Models\State;
+use App\Models\Tag;
+use Database\Factories\ArticleFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +19,21 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $tags = Tag::factory(10)->create();
+
+        $articles = Article::factory(20)->create();
+
+        $tags_id = $tags->pluck('id');
+
+        $articles->each(function ($article) use ($tags_id) {
+            $article->tags()->attach($tags_id->random(3));
+            Comment::factory(3)->create([
+                'article_id' => $article->id
+            ]);
+
+            State::factory(1)->create([
+                'article_id' => $article->id
+            ]);
+        });
     }
 }
